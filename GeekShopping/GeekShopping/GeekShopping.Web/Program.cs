65ff5f3653ad765c90f6112164af91d75c6e4696
leonardolucusi <lucusihiu@gmail.com>
2024-06-20 15:07:14 -0300

    feat: HttpExtensions added

--- GeekShopping/GeekShopping/GeekShopping.Web/Program.cs
+++ GeekShopping/GeekShopping/GeekShopping.Web/Program.cs
@@ -1,3 +1,6 @@
+using GeekShopping.Web.Services;
+using GeekShopping.Web.Services.IServices;
+
 namespace GeekShopping.Web
 {
     public class Program
@@ -6,12 +9,14 @@ namespace GeekShopping.Web
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
+            builder.Services.AddHttpClient<IProductService, ProductService>(
+                c => c.BaseAddress = new Uri(builder.Configuration["ServiceUrls:ProductAPI"])
+            );
+
             builder.Services.AddControllersWithViews();
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
             if (!app.Environment.IsDevelopment())
             {
                 app.UseExceptionHandler("/Home/Error");
